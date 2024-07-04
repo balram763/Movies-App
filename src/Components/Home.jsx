a373f72d798960a4eff82eb7ac3c485a80e3e6ed
balram763 <balramdhakad763@gmail.com>
2024-07-04 15:33:26 +0530

    first commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 import Card from './Card'
 
 const Home = ({movie}) => {
+
+
+
+  if(movie.length === 0){
+    return(
+      <>
+      
+      <div className="text-center d-flex align-item-center justify-content-center container p-5"> 
+     <button className="btn btn-primary " type="button" disabled>
+      <span className="spinner-border spinner-border-sm" aria-hidden="true"></span>
+      <span role="status"> Loading.....</span>
+     </button>
+     </div></>
+    )
+  }
+
+
   return (
     <>
         <div className="container p-3 text-center my-2">
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 const Navbar = () => {
+
+  
   return (
     <>
-    <nav class="navbar bg-primary">
-  <div class="container">
-    <h3 class="navbar-brand mb-0 h1 text-light">Movie</h3>
+    <nav className="navbar bg-primary">
+  <div className="container d-flex justify-content-between">
+    <h3 className="navbar-brand mb-0 h1 text-light">Movie</h3>
+
+    <div>
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button class="btn btn-outline-light" type="submit">Search</button>
+    </form>
+    </div>
   </div>
 </nav>
     
